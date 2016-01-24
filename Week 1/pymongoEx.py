3114ad34cbb44163ab9f8869fd0c65d810560e5c
cayres <rcayres18@gmail.com>
2016-01-24 03:47:56 -0300

    Pymongo Example

---
+++ Week 1/pymongoEx.py
@@ -0,0 +1,14 @@
+import pymongo
+
+from pymongo import MongoClient
+
+#Connect to database
+connection = MongoClient('localhost', 27017)
+
+db = connection.test
+
+names = db.names
+
+item = names.find_one()
+
+print(item['name'])
